485fa322f2dc1b560a1dc80a304f479e54fe6939
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-09-25 20:15:39 +0800

    Add game() function
    game() function loops playRound() 5x and
    calculates the score for each player.
    Will show who wins in the end.

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+let computerScore;
+let playerScore;
+
 function getComputerChoice() {
     let randNum = Math.floor((Math.random()*3)+1);
     switch (randNum) {
@@ -25,27 +30,46 @@ function playRound (playerSelection, computerSelection){
     }
     else if(playerSelection == 'rock'){
         if(computer == 'paper'){
+            computerScore++;
             return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }else{
+            playerScore++;
             return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }
     }
     else if(playerSelection == 'scissors'){
         if(computer == 'rock'){
+            computerScore++;
             return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }else{
+            playerScore++;
             return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }
     }
     else if(playerSelection == 'paper'){
         if(computer == 'scissors'){
+            computerScore++;
             return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }else{
+            playerScore++
             return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
         }
     }
 
 }
 
+function game(){
+    
+    for(let i=0; i<5; i++){
+        console.log("Round " + (i+1));
+        console.log(playRound(getPlayerChoice(),getComputerChoice()));
+    }
 
-console.log(playRound(getPlayerChoice(),getComputerChoice()));
+    return (playerScore==computerScore)?"TIE !! Nobody wins ": (playerScore>computerScore)?"Player WON!!!":"Computer WON!!! "; 
+
+
+
+
+}
+
+console.log(game());
