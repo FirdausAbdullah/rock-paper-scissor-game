2a0a43b0ad478ad300ea9869b5509f80c19754d0
ddoabdullahXubuntu <mfirdaus.tch@gmail.com>
2023-09-25 18:19:12 +0800

    Add getPlayerChoice and playRound function
    getPlayerChoice will prompt user for their choice.
    playRound will compare both player's and computer's
    choice and decides who wins.

--- script.js
+++ script.js
@@ -6,8 +6,46 @@ function getComputerChoice() {
         case 2:
             return "Paper";
         case 3: 
-            return "Scissor";
+            return "Scissors";
     }
 }
 
-console.log(getComputerChoice());
+function getPlayerChoice() {
+    let playerChoice = prompt("Enter your choice. Rock/Paper/Scissors").toLowerCase();
+    return playerChoice;
+} 
+
+
+
+function playRound (playerSelection, computerSelection){
+
+    let computer = computerSelection.toLowerCase();
+    if(playerSelection === computer){
+        return 'Tie! '.concat("player:"+playerSelection+" "+"computer:"+computer);
+    }
+    else if(playerSelection == 'rock'){
+        if(computer == 'paper'){
+            return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }else{
+            return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }
+    }
+    else if(playerSelection == 'scissors'){
+        if(computer == 'rock'){
+            return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }else{
+            return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }
+    }
+    else if(playerSelection == 'paper'){
+        if(computer == 'scissors'){
+            return 'Computer Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }else{
+            return 'Player Won!  '.concat("player:"+playerSelection+" "+"computer:"+computer);
+        }
+    }
+
+}
+
+
+console.log(playRound(getPlayerChoice(),getComputerChoice()));
